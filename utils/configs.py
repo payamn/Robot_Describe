24b33e0f55e5aac7a06ebcab71f05bade9c5c374
Payam Nikdel <pnikdel@sfu.ca>
2018-09-05 21:26:48 -0700

    fixed param and new map with corridor

--- utils/configs.py
+++ utils/configs.py
@@ -1,6 +1,6 @@
-MAP_NAME = "real_2"
-X_OFFSET = -30.150
-Y_OFFSET = -26.500
+MAP_NAME = "combinenormal"
+X_OFFSET = -67.5
+Y_OFFSET = -19.875
 
 MODE = "train" # train or test
 
@@ -10,7 +10,7 @@ To Run the code for new map:
 
 1) run generate map with generate_point argument to first annotate intersections
 2) run generate path with is_generate_path = True and move robot in stage to generate path
-3) run generate map withour generate_point to generate data
+3) run generate map withour generate_point to generate  qdata
 
 MAP_NAME = "aces_map"
 X_OFFSET = -44.2
